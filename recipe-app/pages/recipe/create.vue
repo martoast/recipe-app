e40784f1a58f0b1eb758d3f88402a377556c7e1b
Alex Martos <alexmartos96@gmail.com>
2019-06-10 16:51:00 -0700

    available ingredients autocomplete ui

--- recipe-app/pages/recipe/create.vue
+++ recipe-app/pages/recipe/create.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1> Create a Recipe!</h1>
+    <pre>{{ categorizedIngredients }}</pre>
     <v-form ref="form">
       <v-text-field
         v-model="name"
@@ -56,6 +57,159 @@
       </nuxt-link>
 
     </v-form>
+    <!-- <v-card
+      color="blue-grey darken-1"
+      dark
+    >
+      <v-img
+        height="200"
+        src="https://cdn.vuetifyjs.com/images/cards/dark-beach.jpg"
+      >
+        <v-layout wrap>
+          <v-flex xs12>
+            <v-progress-linear
+              :active="isUpdating"
+              class="ma-0"
+              color="green lighten-3"
+              height="4"
+              indeterminate
+            ></v-progress-linear>
+          </v-flex>
+          <v-flex
+            text-xs-right
+            xs12
+          >
+            <v-menu
+              bottom
+              left
+              transition="slide-y-transition"
+            >
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  v-on="on"
+                >
+                  <v-icon>more_vert</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-tile @click="isUpdating = true">
+                  <v-list-tile-action>
+                    <v-icon>mdi-settings</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                    <v-list-tile-title>Update</v-list-tile-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-menu>
+          </v-flex>
+          <v-layout
+            align-start
+            column
+            justify-end
+            pa-3
+          >
+            <h3 class="headline">{{ name }}</h3>
+            <span class="grey--text text--lighten-1">{{ title }}</span>
+          </v-layout>
+        </v-layout>
+      </v-img>
+      <v-form>
+        <v-container>
+          <v-layout wrap>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="name"
+                :disabled="isUpdating"
+                box
+                color="blue-grey lighten-2"
+                label="Name"
+              ></v-text-field>
+            </v-flex>
+            <v-flex
+              xs12
+              md6
+            >
+              <v-text-field
+                v-model="title"
+                :disabled="isUpdating"
+                box
+                color="blue-grey lighten-2"
+                label="Title"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-autocomplete
+                v-model="friends"
+                :disabled="isUpdating"
+                :items="people"
+                box
+                chips
+                color="blue-grey lighten-2"
+                label="Select"
+                item-text="name"
+                item-value="name"
+                multiple
+              >
+                <template v-slot:selection="data">
+                  <v-chip
+                    :selected="data.selected"
+                    close
+                    class="chip--select-multi"
+                    @input="remove(data.item)"
+                  >
+                    <v-avatar>
+                      <img :src="data.item.avatar">
+                    </v-avatar>
+                    {{ data.item.name }}
+                  </v-chip>
+                </template>
+                <template v-slot:item="data">
+                  <template v-if="typeof data.item !== 'object'">
+                    <v-list-tile-content v-text="data.item"></v-list-tile-content>
+                  </template>
+                  <template v-else>
+                    <v-list-tile-avatar>
+                      <img :src="data.item.avatar">
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                      <v-list-tile-title v-html="data.item.name"></v-list-tile-title>
+                      <v-list-tile-sub-title v-html="data.item.group"></v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </template>
+                </template>
+              </v-autocomplete>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-form>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-switch
+          v-model="autoUpdate"
+          :disabled="isUpdating"
+          class="mt-0"
+          color="green lighten-2"
+          hide-details
+          label="Auto Update"
+        ></v-switch>
+        <v-spacer></v-spacer>
+        <v-btn
+          :disabled="autoUpdate"
+          :loading="isUpdating"
+          color="blue-grey darken-3"
+          depressed
+          @click="isUpdating = true"
+        >
+          <v-icon left>mdi-update</v-icon>
+          Update Now
+        </v-btn>
+      </v-card-actions>
+    </v-card> -->
   </div>
 
 </template>
@@ -63,6 +217,7 @@
 <script>
 import { mapActions } from 'vuex'
 import { mapState } from 'vuex'
+const _ = require('lodash')
 export default {
   data() {
     return {
@@ -77,7 +232,18 @@ export default {
   computed: {
     ...mapState({
       ingredientsStateValue: state => state.recipes.ingredients
-    })
+    }),
+
+    categorizedIngredients() {
+      let returnItems = []
+      const meats = this.availableIngredients.filter(this.isMeat)
+      const grains = this.availableIngredients.filter(this.isGrains)
+      const header1 = 'Group 1'
+      const header2 = 'Group 2'
+
+      returnItems = _.concat(returnItems, header1, meats, header2, grains)
+      return returnItems
+    }
   },
   async fetch({ store, error }) {
     try {
@@ -116,6 +282,12 @@ export default {
           // console.log('There was a probelm creating Recipe')
           console.error(err)
         })
+    },
+    isMeat(availableIngredients) {
+      return availableIngredients.group === 'meat'
+    },
+    isGrains(availableIngredients) {
+      return availableIngredients.group === 'grains'
     }
   }
 }
