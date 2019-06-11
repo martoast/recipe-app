32849742ccd73af0d97cc05c0cc50ef42fe829dd
Alex Martos <alexmartos@gmail.com>
2019-06-10 17:27:34 -0700

    autocomplete for ingredients complete

--- recipe-app/pages/recipe/create.vue
+++ recipe-app/pages/recipe/create.vue
@@ -1,152 +1,18 @@
 <template>
   <div>
-    <h1> Create a Recipe!</h1>
+    <h1>Create a Recipe!</h1>
     <pre>{{ categorizedIngredients }}</pre>
     <v-form ref="form">
-      <v-text-field
-        v-model="name"
-        label="Name"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="img"
-        label="Image URL"
-        required
-      ></v-text-field>
+      <v-text-field v-model="name" label="Name" required></v-text-field>
+      <v-text-field v-model="img" label="Image URL" required></v-text-field>
 
-      <v-container fluid>
-        <v-layout wrap>
-          <v-flex xs12>
-            <v-combobox
-              v-model="ingredients"
-              :items="availableIngredients"
-              label="Select all necesarry ingredients"
-              multiple
-            ></v-combobox>
-          </v-flex>
-        </v-layout>
-      </v-container>
-      <v-container
-        fluid
-        grid-list-xl
-      >
-        <v-layout
-          wrap
-          align-center
-        >
-          <v-flex
-            xs12
-            sm6
-            d-flex
-          >
-            <v-select
-              v-model="region"
-              :items="regions"
-              label="Region"
-            ></v-select>
-          </v-flex>
-        </v-layout>
-      </v-container>
-      <nuxt-link to="/">
-        <v-btn
-          color="success"
-          @click="submit"
-        >
-          Submit
-        </v-btn>
-      </nuxt-link>
-
-    </v-form>
-    <!-- <v-card
-      color="blue-grey darken-1"
-      dark
-    >
-      <v-img
-        height="200"
-        src="https://cdn.vuetifyjs.com/images/cards/dark-beach.jpg"
-      >
-        <v-layout wrap>
-          <v-flex xs12>
-            <v-progress-linear
-              :active="isUpdating"
-              class="ma-0"
-              color="green lighten-3"
-              height="4"
-              indeterminate
-            ></v-progress-linear>
-          </v-flex>
-          <v-flex
-            text-xs-right
-            xs12
-          >
-            <v-menu
-              bottom
-              left
-              transition="slide-y-transition"
-            >
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  v-on="on"
-                >
-                  <v-icon>more_vert</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-tile @click="isUpdating = true">
-                  <v-list-tile-action>
-                    <v-icon>mdi-settings</v-icon>
-                  </v-list-tile-action>
-                  <v-list-tile-content>
-                    <v-list-tile-title>Update</v-list-tile-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-              </v-list>
-            </v-menu>
-          </v-flex>
-          <v-layout
-            align-start
-            column
-            justify-end
-            pa-3
-          >
-            <h3 class="headline">{{ name }}</h3>
-            <span class="grey--text text--lighten-1">{{ title }}</span>
-          </v-layout>
-        </v-layout>
-      </v-img>
       <v-form>
         <v-container>
-          <v-layout wrap>
-            <v-flex
-              xs12
-              md6
-            >
-              <v-text-field
-                v-model="name"
-                :disabled="isUpdating"
-                box
-                color="blue-grey lighten-2"
-                label="Name"
-              ></v-text-field>
-            </v-flex>
-            <v-flex
-              xs12
-              md6
-            >
-              <v-text-field
-                v-model="title"
-                :disabled="isUpdating"
-                box
-                color="blue-grey lighten-2"
-                label="Title"
-              ></v-text-field>
-            </v-flex>
+          <v-layout>
             <v-flex xs12>
               <v-autocomplete
-                v-model="friends"
-                :disabled="isUpdating"
-                :items="people"
+                v-model="ingredients"
+                :items="categorizedIngredients"
                 box
                 chips
                 color="blue-grey lighten-2"
@@ -187,31 +53,18 @@
           </v-layout>
         </v-container>
       </v-form>
-      <v-divider></v-divider>
-      <v-card-actions>
-        <v-switch
-          v-model="autoUpdate"
-          :disabled="isUpdating"
-          class="mt-0"
-          color="green lighten-2"
-          hide-details
-          label="Auto Update"
-        ></v-switch>
-        <v-spacer></v-spacer>
-        <v-btn
-          :disabled="autoUpdate"
-          :loading="isUpdating"
-          color="blue-grey darken-3"
-          depressed
-          @click="isUpdating = true"
-        >
-          <v-icon left>mdi-update</v-icon>
-          Update Now
-        </v-btn>
-      </v-card-actions>
-    </v-card> -->
+      <v-container fluid grid-list-xl>
+        <v-layout wrap align-center>
+          <v-flex xs12 sm6 d-flex>
+            <v-select v-model="region" :items="regions" label="Region"></v-select>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <nuxt-link to="/">
+        <v-btn color="success" @click="submit">Submit</v-btn>
+      </nuxt-link>
+    </v-form>
   </div>
-
 </template>
 
 <script>
@@ -238,8 +91,8 @@ export default {
       let returnItems = []
       const meats = this.availableIngredients.filter(this.isMeat)
       const grains = this.availableIngredients.filter(this.isGrains)
-      const header1 = 'Group 1'
-      const header2 = 'Group 2'
+      const header1 = { header: 'Group 1' }
+      const header2 = { header: 'Group 2' }
 
       returnItems = _.concat(returnItems, header1, meats, header2, grains)
       return returnItems
@@ -288,6 +141,10 @@ export default {
     },
     isGrains(availableIngredients) {
       return availableIngredients.group === 'grains'
+    },
+    remove(item) {
+      const index = this.friends.indexOf(item.name)
+      if (index >= 0) this.friends.splice(index, 1)
     }
   }
 }
